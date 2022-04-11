8494a65d6933999a87920bd6083a10b6d942ad7a
Matthew Kirkendall <kirk.viola@gmail.com>
2022-04-10 21:44:31 -0400

    built item model class and service

---
+++ src/app/item-models/item-model.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SystemService } from '../system.service';
+import { ItemModel } from './item-model.class';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemModelService {
+
+  baseUrl: string = this.sysSvc.baseUrl + '/itemmodels'
+  constructor(private sysSvc: SystemService,
+              private http: HttpClient) { }
+
+  list(): Observable<ItemModel[]> {
+    return this.http.get(this.baseUrl) as Observable<ItemModel[]>;
+  }
+
+  getById(id: number): Observable<ItemModel> {
+    return this.http.get(`${this.baseUrl}/${id}`) as Observable<ItemModel>;
+  }
+
+  create(model: ItemModel): Observable<ItemModel> {
+    return this.http.post(this.baseUrl, model) as Observable<ItemModel>;
+  }
+
+  update(model: ItemModel): Observable<ItemModel> {
+    return this.http.put(`${this.baseUrl}/${model.id}`, model) as Observable<ItemModel>;
+  }
+
+  remove(id: number): Observable<ItemModel> {
+    return this.http.delete(`${this.baseUrl}/${id}`) as Observable<ItemModel>;
+  }
+}
